72612f4133e1710058967152d3e83fc0347e1d7b
treygardner-dev <treygardner.dev@protonmail.com>
2026-03-29 00:00:42 -0600

    setting up outline for C# / .net MAUI files

--- study-wiki/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/study-wiki.AssemblyInfo.cs
+++ study-wiki/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/study-wiki.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("study-wiki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+23d2aa235ad875718e809f136e2587a6013dcf56")]
 [assembly: System.Reflection.AssemblyProductAttribute("study-wiki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("study-wiki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
